630df4f13bb8eac4c6360ec7a123c28944f6b095
Jesse Griffin <jesse.griffin@gmail.com>
2020-01-24 03:14:25 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot("Pretty Bird!")
+  phrase = "Squawk!"
+  puts phrase
+  return "Squawk!"
+end
+
+parrot
+
+
